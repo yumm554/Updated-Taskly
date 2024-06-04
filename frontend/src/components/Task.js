3fcac29236bb722a8e9351ad8cd93614e785fd47
Yum <yumansaeed777@gmail.com>
2024-06-04 18:49:04 +0500

    date added in tasks

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -12,43 +12,46 @@ function Task(props) {
   const { _id: taskId, name, completed } = props;
   return (
     <div className="lists">
-      {completed ? (
-        <div className="completed-div">
-          <Completed />
-          <p className="completed">{name}</p>
-        </div>
-      ) : (
-        <p>{name}</p>
-      )}
-      <div className="list-icons">
-        {(isDeleteLoading && <div className="loader"></div>) ||
-          (isDeleteError && <p className="error">error</p>)}
-        <div
-          className="transparent-btn"
-          onClick={() => {
-            setIsDeleteLoading(true);
-            setTask(props);
-            navigate(`${taskId}`);
-          }}
-        >
-          <Edit />
-        </div>
-        <div
-          className="transparent-btn"
-          onClick={() => {
-            setIsDeleteLoading(true);
-            deleteTask(taskId)
-              .then((resp) => {
-                setIsDeleteLoading(false);
-                setGetAgain(!getAgain);
-              })
-              .catch((err) => {
-                setIsDeleteError(true);
-                setIsDeleteLoading(false);
-              });
-          }}
-        >
-          <Delete />
+      <div className="list-task-date">24 may 24</div>
+      <div className="list-task-content">
+        {completed ? (
+          <div className="completed-div">
+            <Completed />
+            <p className="completed">{name}</p>
+          </div>
+        ) : (
+          <p>{name}</p>
+        )}
+        <div className="list-icons">
+          {(isDeleteLoading && <div className="loader"></div>) ||
+            (isDeleteError && <p className="error">error</p>)}
+          <div
+            className="transparent-btn"
+            onClick={() => {
+              setIsDeleteLoading(true);
+              setTask(props);
+              navigate(`${taskId}`);
+            }}
+          >
+            <Edit />
+          </div>
+          <div
+            className="transparent-btn"
+            onClick={() => {
+              setIsDeleteLoading(true);
+              deleteTask(taskId)
+                .then((resp) => {
+                  setIsDeleteLoading(false);
+                  setGetAgain(!getAgain);
+                })
+                .catch((err) => {
+                  setIsDeleteError(true);
+                  setIsDeleteLoading(false);
+                });
+            }}
+          >
+            <Delete />
+          </div>
         </div>
       </div>
     </div>
