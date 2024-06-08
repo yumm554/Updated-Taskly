3b341604d5b786a1df8d0501c862180bef652f03
Yum <yumansaeed777@gmail.com>
2024-06-08 09:23:59 +0500

    some design changes

--- backend/controllers/tasksControllers.js
+++ backend/controllers/tasksControllers.js
@@ -17,14 +17,15 @@ const createTask = async (req, res) => {
   try {
     const { name, email, dateCreated } = await req.body;
     if (!name) {
-      return res.status(400).json({ message: 'must provide name' });
+      return res.status(400).json('must provide name');
     }
 
     const task = await Task.create({ name, email, dateCreated });
     console.log({ task });
-    return res.status(200).json({ message: `Task created` });
+    return res.status(200).json(`Added, succesfully`);
   } catch (err) {
     console.log(err);
+    return res.status(500).json('An error occurred');
   }
 };
 
@@ -48,11 +49,12 @@ const updateTask = async (req, res) => {
 
     const task = await Task.findOneAndUpdate({ _id: taskID }, req.body);
     if (!task) {
-      res.status(404).json({ message: 'user not found' });
+      res.status(404).json('task not found');
     }
-    res.status(200).json('Task updated');
+    res.status(200).json('Saved, succesfully');
   } catch (err) {
     console.log(err);
+    return res.status(500).json('An error occurred');
   }
 };
 
@@ -62,11 +64,12 @@ const deleteTask = async (req, res) => {
 
     const task = await Task.findOneAndDelete({ _id: taskID });
     if (!task) {
-      res.status(404).json({ message: 'user not found' });
+      res.status(404).json('not found');
     }
-    res.status(200).json('Task deleted');
+    res.status(200).json('success');
   } catch (err) {
     console.log(err);
+    return res.status(500).json('error');
   }
 };
 
